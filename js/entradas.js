826fe6933ae858f106da249d07945fddd91d9ae7
santiange <santiangelica410@gmail.com>
2023-11-21 19:40:32 -0300

    historial de entradas

--- js/entradas.js
+++ js/entradas.js
@@ -13,6 +13,58 @@ const plus = document.querySelector(".plus")
 const minus = document.querySelector(".minus")
 const num = document.querySelector(".num")
 let sumador = 1;
+const boxHistorial = document.getElementById("historialentradas")
+const hora = new Date()
+
+//CREAR CARD EN HISTORIAL DE ENTRADAS
+
+function CardHistorial(entrada, dato) {
+
+    if (!dato) {
+        entrada.forEach(entrada => {
+            for (let i = 1; i <= entrada.cantidad; i += 1) {
+                const nuevaCard = document.createElement("div")
+                nuevaCard.classList.add("cardHistorial")
+                nuevaCard.classList.add("container")
+                nuevaCard.classList.add("mb-5")
+                nuevaCard.innerHTML = `
+                <figure class="figure-card-historial">
+                    <img src="../media/ticket.png" class="img-ticket" alt="">
+                </figure>
+                <div class="contenido-card-historial">
+                    <div class="box-contenido-historial mb-0">
+                        <h5 class="title-card-historial mb-0">${entrada.nombre}</h5>
+                        <p class="mb-0">${entrada.fecha}</p>
+                    </div>
+                    <p class="text-card-historial mb-0">$${entrada.precio}</p>      
+                </div>
+                `
+                boxHistorial.appendChild(nuevaCard)
+            }
+        })
+    }
+    else {
+        for (let i = 1; i <= entrada.cantidad; i += 1) {
+            const nuevaCard = document.createElement("div")
+            nuevaCard.classList.add("cardHistorial")
+            nuevaCard.classList.add("container")
+            nuevaCard.classList.add("mb-5")
+            nuevaCard.innerHTML = `
+        <figure class="figure-card-historial">
+            <img src="../media/ticket.png" class="img-ticket" alt="">
+        </figure>
+        <div class="contenido-card-historial">
+            <div class="box-contenido-historial mb-0">
+                <h5 class="title-card-historial mb-0">${entrada.nombre}</h5>
+                <p class="mb-0">${entrada.fecha}</p>
+            </div>
+            <p class="text-card-historial mb-0">$${entrada.precio}</p>      
+        </div>
+        `
+            boxHistorial.appendChild(nuevaCard)
+        }
+    }
+}
 
 
 
@@ -46,7 +98,34 @@ function CrearEntrada(entradas) {
     })
 }
 
+
+
+function AñadirEntradaAlCarrito(entrada) {
+    const arr = JSON.parse(localStorage.getItem("entradas"))
+    entrada.fecha = 
+        `${hora.getDate()}/${hora.getMonth() + 1}/${hora.getFullYear()} - ${hora.getHours()}:${hora.getMinutes()}:${hora.getSeconds()} `
+
+    if (!arr) {
+        entrada.id = 1;
+        const nuevaEntrada = entrada
+        localStorage.setItem("entradas", JSON.stringify([nuevaEntrada]))
+    }
+    else {
+        const NuevoArr = arr
+        entrada.id = NuevoArr.length + 1
+        NuevoArr.push(entrada)
+        localStorage.setItem("entradas", JSON.stringify(NuevoArr))
+    }
+    CardHistorial(entrada, true)
+}
+
+
 CrearEntrada(ArrEntradas)
+const EntradasMostrar = JSON.parse(localStorage.getItem("entradas"))
+if (EntradasMostrar) {
+    CardHistorial(EntradasMostrar, false)
+}
+
 
 
 //CHEQUEAR SOCIO, CANTIDAD Y MOSTRAR
@@ -54,12 +133,14 @@ formModal.addEventListener('submit', (evt) => {
     evt.preventDefault()
     let objActual = ArrEntradas.find(x => x.id === id)
     alerta.classList.add("azul")
+    alerta.classList.add("alerta")
     modalAprobed.innerText = "APROBADO!"
     modalCant.innerText = `x${sumador}`
     modalNombre.innerText = objActual.nombre
     modalPrecio.innerText = `$${objActual.precio * sumador}`
-
 })
+
+
 
 
 //CERRAMOS MODAL Y REINICIAMOS
@@ -80,21 +161,24 @@ cerrarModalEntradas.forEach(x => {
 
 //añadir entrada al carrito
 añadirentrada.addEventListener('click', () => {
-    console.log( modalNombre.innerText +  modalPrecio.innerText +  modalCant.innerText)
-    AgregarAlCarrito(
+    const objCrear = ArrEntradas.find(x => x.nombre === modalNombre.innerText)
+    AñadirEntradaAlCarrito(
         {
-            nombre: modalNombre.innerText,
-            precio: Number(modalPrecio.innerText) * sumador,
-            cantidad: modalCant.innerText,
-            src: "../media/ticket.jpg"
+            nombre: objCrear.nombre,
+            precio: objCrear.precio,
+            cantidad: sumador
         }
     )
+
     sumador = 1
-        num.innerText = `0${sumador}`
-        modalCant.innerText = ""
-        modalNombre.innerText = ""
-        modalPrecio.innerText = ""
+    alerta.removeAttribute("class", "azul")
+    num.innerText = `0${sumador}`
+    modalAprobed.innerText = ""
+    modalCant.innerText = ""
+    modalNombre.innerText = ""
+    modalPrecio.innerText = ""
 })
+
 
 
 
@@ -103,7 +187,6 @@ plus.addEventListener('click', () => {
     sumador < 3 && (sumador += 1)
     num.innerText = `0${sumador}`
 })
-
 minus.addEventListener('click', () => {
     sumador > 1 && (sumador -= 1)
     num.innerText = `0${sumador}`
